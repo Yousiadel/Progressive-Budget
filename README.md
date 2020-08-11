0ae954d2ff77f091fd84a93d993bc0db9b4a2265
Youstina Ibrahim <youstinaaibrahim@gmail.com>
2020-08-11 19:54:43 -0400

    Progressive

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Progressive-Budget
+# On the Go Budget Tracker
+
+## Description 
+
+Budget tracker application that allows users to enter deposits and expenses. Transactions can be entered online and offline, all data will sync whenever online. Functionality based on connectivity is broken down into the following: 
+
+Offline Functionality:
+
+  * Enter deposits offline
+
+  * Enter expenses offline
+
+When brought back online:
+
+  * Offline entries should be added to tracker.
+
+## User Story
+AS AN avid traveller
+I WANT to be able to track my withdrawals and deposits with or without a data/internet connection
+SO THAT my account balance is accurate when I am traveling
+
+## Business Context
+
+Giving users a fast and easy way to track their money is important, but allowing them to access that information anytime is even more important. Having offline functionality is paramount to our applications success.
+
+
--- server.js
+++ server.js
@@ -1,34 +1,30 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
+const compression = require("compression");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-let dataUri = "mongodb://localhost/budget";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
-if (process.env.MONGODB_URI) {
-
-    mongoose.connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useFindAndModify: false
-    });
-} else {
-
-    mongoose.connect(dataUri);
-}
-
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}!`);
 });
